f6dae8495622573c22b5352201fa17250f818680
Daniel <daniel001.dhf@gmail.com>
2024-04-17 11:44:06 +0800

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListComponent } from './list/list.component';
+import { ListItemComponent } from './list-item/list-item.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PaginationModule } from './pagination/pagination.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListComponent,
+    ListItemComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    PaginationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
